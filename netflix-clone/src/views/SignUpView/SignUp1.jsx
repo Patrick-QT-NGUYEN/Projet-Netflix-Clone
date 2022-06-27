2fb1a29f2ecf600e12c8dc764cb986cfe2fa8e94
Soukaïna AZARZAR <soukaina.az59@gmail.com>
2022-06-27 16:43:50 +0200

    ajout pages SignUP1 et SignUp2

---
+++ netflix-clone/src/views/SignUpView/SignUp1.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './SignUp1.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const SignUp1 = () => {
+    return (
+<div className="card configuration">
+  <img src="./img/SignUP_Page1_Screens.jpg" className="card-img-top" alt="..." />
+  <div className="card-body">
+    <h5 className="card-title">ETAPE 1 SUR 3</h5>
+    <h1 className="card-title"> Complétez la configuration de votre compte </h1>
+    <p className="card-text">Netflix est personnalisé selon vos <br/> goûts. Créez un mot de passe pour <br/> regarder Netflix quand vous voulez <br/>, où vous voulez.</p>
+    <a href="#" className="btn btn-danger form-control">Suivant</a>
+  </div>
+</div>
+    );
+};
+
+export default SignUp1;
