3495e7dff014cf06b25469f348f336390803026e
Soukaïna AZARZAR <soukaina.az59@gmail.com>
2022-06-28 15:01:10 +0200

    Sign UP Soukaïna Part

--- netflix-clone/src/views/SignUpView/SignUp3.jsx
+++ netflix-clone/src/views/SignUpView/SignUp3.jsx
@@ -10,13 +10,15 @@ const SignUp3 = () => {
                     <h5 className="card-title">ETAPE 3 SUR 4</h5>
                     <h2 className="card-title"> Créez un mot de passe pour démarrer votre abonnement</h2>
                     <p className="card-text">Plus que quelques étapes et c'est fini ! <br /> Plus rien à remplir.</p>
-                    <input placeholder='E-mail' type="email"></input> <br />
-                    <input placeholder='Ajouter un mot de passe' type="password"></input>
+                    <input className='config' placeholder='E-mail' type="email"></input> <br />
+                    <input className='config' placeholder='Ajouter un mot de passe' type="password"></input>
+                    <br />
                     <div class="form-check">
-                        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked/>
+                        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" />
                             <label class="form-check-label" for="flexCheckChecked">
-                                Oui, envoyez-moi les offres spéciales de Netflix par email.
+                                Oui, envoyez-moi les offres spéciales de Netflix par email. <br />
                             </label>
+                            
                     </div>
                     <a href="/SignUp4" className="btn btn-danger form-control">Suivant</a>
                 </div>
